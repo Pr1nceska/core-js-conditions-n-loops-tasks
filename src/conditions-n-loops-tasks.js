9e46b074af8bef7a0b0743070cb93c180a90c2bf
Pr1nceska <lubitel.priklu4eniy1103@gmail.com>
2025-01-12 16:18:12 +0300

    feat: 3 task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -73,8 +73,11 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x || queen.y === king.y) return true;
+
+  if (Math.abs(queen.y - king.y) === Math.abs(queen.x - king.x)) return true;
+  return false;
 }
 
 /**
